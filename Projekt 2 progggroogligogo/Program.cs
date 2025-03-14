3e88a28786713c891b9ce06e9ee37b2dc14498fb
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2025-03-14 13:17:18 +0100

    spara

--- Projekt 2 progggroogligogo/Program.cs
+++ Projekt 2 progggroogligogo/Program.cs
@@ -12,3 +12,27 @@ Motståndare kebabMan =new();
 kebabMan.Hp = 150;
 kebabMan.skada = [20, Random.Shared.Next(5,40), 50];
 kebabMan.attacker = ["kebabspett", "köttklump", "bepsi"];
+
+
+System.Console.WriteLine("                                                      Bertils Liv");
+System.Console.WriteLine("""
+                            __________               __  .__.__    ___.                 
+                            \______   \ ____________/  |_|__|  |   \_ |__   ____ ___.__.
+                             |    |  _// __ \_  __ \   __\  |  |    | __ \ /  _ <   |  |
+                             |    |   \  ___/|  | \/|  | |  |  |__  | \_\ (  <_> )___  |
+                             |______  /\___  >__|   |__| |__|____/  |___  /\____// ____|
+                                    \/     \/                           \/       \/    
+""");
+
+while(true){
+    System.Console.WriteLine("Är du redo att börja");
+    if(){
+
+    }
+}
+
+System.Console.WriteLine("Ditt namn är Bertil");
+System.Console.WriteLine("Du vaknar upp en vanlig morgon, dricker ditt kaffe och går ut");
+
+
+Console.ReadLine();
